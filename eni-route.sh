c506ad0b73cfa1b0c27c990c0981b5102337284f
s2977943 <w.dovey@griffith.edu.au>
2016-05-10 09:16:19 +1000

    permanent and cleanup

--- eni-route.sh
+++ eni-route.sh
@@ -8,11 +8,11 @@
 #	exit 1
 #}
 
-#yum install NetworkManager-config-routing-rules -y
-#systemctl enable NetworkManager-dispatcher.service
-#systemctl start NetworkManager-dispatcher.service
+yum install NetworkManager-config-routing-rules -y
+systemctl enable NetworkManager-dispatcher.service
+systemctl start NetworkManager-dispatcher.service
 
-dhclient
+#dhclient
 
 ETH0_MAC=$(cat /sys/class/net/eth0/address) ||
     die "Unable to determine MAC address on eth0."
@@ -22,52 +22,52 @@ ETH1_MAC=$(cat /sys/class/net/eth1/address) ||
     die "Unable to determine MAC address on eth1."
 echo "Found MAC ${ETH1_MAC} for eth1."
 
-VPC_CIDR_URI_ETH0="http://169.254.169.254/latest/meta-data/network/interfaces/macs/${ETH0_MAC}/subnet-ipv4-cidr-block"
-echo "Metadata location for vpc ipv4 range: ${VPC_CIDR_URI_ETH0}"
+URI_ETH0="http://169.254.169.254/latest/meta-data/network/interfaces/macs/${ETH0_MAC}/subnet-ipv4-cidr-block"
+echo "Metadata location for ipv4 range: ${URI_ETH0}"
 
-VPC_CIDR_RANGE_ETH0=$(curl --retry 3 --silent --fail ${VPC_CIDR_URI_ETH0})
+RANGE_ETH0=$(curl --retry 3 --silent --fail ${URI_ETH0})
 if [ $? -ne 0 ]; then
    echo "Unable to retrive VPC CIDR range from meta-data, using 0.0.0.0/0 instead. PAT may be insecure!"
-   VPC_CIDR_RANGE_ETH0="0.0.0.0/0"
+   RANGE_ETH0="0.0.0.0/0"
 else
-   echo "Retrieved VPC CIDR range ${VPC_CIDR_RANGE_ETH0} from meta-data."
+   echo "Retrieved VPC CIDR range ${RANGE_ETH0} from meta-data."
 fi
 
-VPC_URI_ETH0_IP="http://169.254.169.254/latest/meta-data/network/interfaces/macs/${ETH0_MAC}/local-ipv4s"
-echo "Metadata location for vpc ipv4 add: ${VPC_URI_ETH0_IP}"
+URI_ETH0_IP="http://169.254.169.254/latest/meta-data/network/interfaces/macs/${ETH0_MAC}/local-ipv4s"
+echo "Metadata location for ipv4 add: ${URI_ETH0_IP}"
 
-VPC_ETH0_IP=$(curl --retry 3 --silent --fail ${VPC_URI_ETH0_IP})
+ETH0_IP=$(curl --retry 3 --silent --fail ${URI_ETH0_IP})
 if [ $? -ne 0 ]; then
    echo "Unable to retrive VPC IP from meta-data, using null instead"
-   VPC_ETH0_IP=""
+   ETH0_IP=""
 else
-   echo "Retrieved VPC IP ${VPC_ETH0_IP} from meta-data."
+   echo "Retrieved VPC IP ${ETH0_IP} from meta-data."
 fi
 
-VPC_CIDR_URI_ETH1="http://169.254.169.254/latest/meta-data/network/interfaces/macs/${ETH1_MAC}/subnet-ipv4-cidr-block"
-echo "Metadata location for vpc ipv4 range: ${VPC_CIDR_URI_ETH1}"
+URI_ETH1="http://169.254.169.254/latest/meta-data/network/interfaces/macs/${ETH1_MAC}/subnet-ipv4-cidr-block"
+echo "Metadata location for ipv4 range: ${URI_ETH1}"
 
-VPC_CIDR_RANGE_ETH1=$(curl --retry 3 --silent --fail ${VPC_CIDR_URI_ETH1})
+RANGE_ETH1=$(curl --retry 3 --silent --fail ${URI_ETH1})
 if [ $? -ne 0 ]; then
    echo "Unable to retrive VPC CIDR range from meta-data, using 0.0.0.0/0 instead. PAT may be insecure!"
-   VPC_CIDR_RANGE_ETH1="0.0.0.0/0"
+   RANGE_ETH1="0.0.0.0/0"
 else
-   echo "Retrieved VPC CIDR range ${VPC_CIDR_RANGE_ETH1} from meta-data."
+   echo "Retrieved VPC CIDR range ${RANGE_ETH1} from meta-data."
 fi
 
-VPC_URI_ETH1_IP="http://169.254.169.254/latest/meta-data/network/interfaces/macs/${ETH1_MAC}/local-ipv4s"
-echo "Metadata location for vpc ipv4 add: ${VPC_URI_ETH1_IP}"
+URI_ETH1_IP="http://169.254.169.254/latest/meta-data/network/interfaces/macs/${ETH1_MAC}/local-ipv4s"
+echo "Metadata location for ipv4 add: ${URI_ETH1_IP}"
 
-VPC_ETH1_IP=$(curl --retry 3 --silent --fail ${VPC_URI_ETH1_IP})
+ETH1_IP=$(curl --retry 3 --silent --fail ${URI_ETH1_IP})
 if [ $? -ne 0 ]; then
    echo "Unable to retrive VPC IP from meta-data, using null instead"
-   VPC_ETH1_IP=""
+   ETH1_IP=""
 else
-   echo "Retrieved VPC IP ${VPC_ETH1_IP} from meta-data."
+   echo "Retrieved VPC IP ${ETH1_IP} from meta-data."
 fi
 
-ip route add ${VPC_CIDR_RANGE_ETH0} dev eth0 tab 1
-ip route add ${VPC_CIDR_RANGE_ETH1} dev eth1 tab 2
+ip route add ${RANGE_ETH0} dev eth0 tab 1
+ip route add ${RANGE_ETH1} dev eth1 tab 2
 
 ETH0_ROUTE=$(read _ _ gateway _ < <(ip route list match 0/0); echo "$gateway")
 ETH1_ROUTE=$(read _ _ gateway _ < <(ip route list match 0/0); echo "$gateway")
@@ -75,6 +75,15 @@ ETH1_ROUTE=$(read _ _ gateway _ < <(ip route list match 0/0); echo "$gateway")
 ip route add default via ${ETH0_ROUTE} dev eth0 tab 1
 ip route add default via ${ETH1_ROUTE} dev eth1 tab 2
 
-ip rule add from ${VPC_ETH0_IP}/32 tab 1 priority 100
-ip rule add from ${VPC_ETH1_IP}/32 tab 2 priority 200
+ip rule add from ${ETH0_IP}/32 tab 1 priority 100
+ip rule add from ${ETH1_IP}/32 tab 2 priority 200
 ip route flush cache 
+
+echo "${RANGE_ETH0} dev eth0 tab 1" > /etc/sysconfig/network-scripts/route-eth0
+echo "default via ${ETH0_ROUTE} dev eth0 tab 1" >> /etc/sysconfig/network-scripts/route-eth0
+
+echo "${RANGE_ETH1} dev eth1 tab 2" > /etc/sysconfig/network-scripts/route-eth1
+echo "default via ${ETH1_ROUTE} dev eth1 tab 2" >> /etc/sysconfig/network-scripts/route-eth1
+
+echo "from ${ETH0_IP}/32 tab 1 priority 100" > /etc/sysconfig/network-scripts/rule-eth0
+echo "from ${ETH1_IP}/32 tab 2 priority 200" > /etc/sysconfig/network-scripts/rule-eth1
