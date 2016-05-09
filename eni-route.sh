d2a2db925c436f94fdd011db272fbb6c7da33a77
Wayne Dovey <wdovey@gmail.com>
2016-05-09 07:31:32 -0400

    cleanup

--- eni-route.sh
+++ eni-route.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-function log { logger -t "vpc" -- $1; }
+#function log { logger -t "vpc" -- $1; }
 
-function die {
-	[ -n "$1" ] && log "$1"
-	log "Configuration of HA NAT failed!"
-	exit 1
-}
+#function die {
+#	[ -n "$1" ] && log "$1"
+#	log "Configuration of HA NAT failed!"
+#	exit 1
+#}
 
 yum install NetworkManager-config-routing-rules -y
 systemctl enable NetworkManager-dispatcher.service
@@ -20,7 +20,7 @@ ETH1_MAC=$(cat /sys/class/net/eth1/address) ||
     die "Unable to determine MAC address on eth1."
 log "Found MAC ${ETH1_MAC} for eth1."
 
-VPC_CIDR_URI_ETH0="http://169.254.169.254/latest/meta-data/network/interfaces/macs/${ETH0_MAC}/vpc-ipv4-cidr-block"
+VPC_CIDR_URI_ETH0="http://169.254.169.254/latest/meta-data/network/interfaces/macs/${ETH0_MAC}/subnet-ipv4-cidr-block"
 log "Metadata location for vpc ipv4 range: ${VPC_CIDR_URI_ETH0}"
 
 VPC_CIDR_RANGE_ETH0=$(curl --retry 3 --silent --fail ${VPC_CIDR_URI_ETH0})
@@ -42,6 +42,17 @@ else
    log "Retrieved VPC IP ${VPC_ETH0_IP} from meta-data."
 fi
 
+VPC_CIDR_URI_ETH1="http://169.254.169.254/latest/meta-data/network/interfaces/macs/${ETH1_MAC}/subnet-ipv4-cidr-block"
+log "Metadata location for vpc ipv4 range: ${VPC_CIDR_URI_ETH1}"
+
+VPC_CIDR_RANGE_ETH1=$(curl --retry 3 --silent --fail ${VPC_CIDR_URI_ETH1})
+if [ $? -ne 0 ]; then
+   log "Unable to retrive VPC CIDR range from meta-data, using 0.0.0.0/0 instead. PAT may be insecure!"
+   VPC_CIDR_RANGE_ETH1="0.0.0.0/0"
+else
+   log "Retrieved VPC CIDR range ${VPC_CIDR_RANGE_ETH1} from meta-data."
+fi
+
 VPC_URI_ETH1_IP="http://169.254.169.254/latest/meta-data/network/interfaces/macs/${ETH0_MAC}/local-ipv4s"
 log "Metadata location for vpc ipv4 add: ${VPC_URI_ETH1_IP}"
 
@@ -51,17 +62,6 @@ if [ $? -ne 0 ]; then
    VPC_ETH1_IP=""
 else
    log "Retrieved VPC IP ${VPC_ETH1_IP} from meta-data."
-fi
-
-VPC_CIDR_URI_ETH1="http://169.254.169.254/latest/meta-data/network/interfaces/macs/${ETH1_MAC}/vpc-ipv4-cidr-block"
-log "Metadata location for vpc ipv4 range: ${VPC_CIDR_URI_ETH1}"
-
-VPC_CIDR_RANGE_ETH1=$(curl --retry 3 --silent --fail ${VPC_CIDR_URI_ETH1})
-if [ $? -ne 0 ]; then
-   log "Unable to retrive VPC CIDR range from meta-data, using 0.0.0.0/0 instead. PAT may be insecure!"
-   VPC_CIDR_RANGE_ETH1="0.0.0.0/0"
-else
-   log "Retrieved VPC CIDR range ${VPC_CIDR_RANGE_ETH1} from meta-data."
 fi
 
 ip route add ${VPC_CIDR_RANGE_ETH0} dev eth0 tab 1
